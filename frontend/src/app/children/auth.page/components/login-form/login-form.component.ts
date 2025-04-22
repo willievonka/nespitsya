e5341a0aa8c08e9be9859ecf0686ad89fc4e58f9
EduardKostylev <edik03032005@yandex.ru>
2025-04-22 21:59:46 +0500

    Попытался сделать форму password

--- frontend/src/app/children/auth.page/components/login-form/login-form.component.ts
+++ frontend/src/app/children/auth.page/components/login-form/login-form.component.ts
@@ -1,13 +1,29 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiAccentButtonComponent } from '../../../../components/tui-components/tui-accent-button/tui-accent-button.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TuiFieldErrorPipe, TuiPassword } from '@taiga-ui/kit';
+import { TuiTextfield, TuiError } from '@taiga-ui/core';
+import { AsyncPipe } from '@angular/common';
+
+
 
 
 @Component({
     selector: 'app-login-form',
-    imports: [],
+    imports: [TuiAccentButtonComponent,
+        ReactiveFormsModule,
+        TuiPassword,
+        TuiTextfield,
+        TuiError,
+        TuiFieldErrorPipe,
+        AsyncPipe,],
     templateUrl: './login-form.component.html',
     styleUrl: './login-form.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginFormComponent {
-
+    protected readonly form: FormGroup = new FormGroup({
+        email: new FormControl('',Validators.email),
+        password: new FormControl('',Validators.required)
+    });
 }
