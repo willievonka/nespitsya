b7f601c516175cd289e6121440845bab9905ba46
EduardKostylev <edik03032005@yandex.ru>
2025-04-23 15:27:01 +0500

    Сделал login-form (почти)

--- frontend/src/app/children/auth.page/components/login-form/login-form.component.ts
+++ frontend/src/app/children/auth.page/components/login-form/login-form.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TuiAccentButtonComponent } from '../../../../components/tui-components/tui-accent-button/tui-accent-button.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TuiFieldErrorPipe, TuiPassword } from '@taiga-ui/kit';
+import { TuiFieldErrorPipe, TuiPassword, tuiValidationErrorsProvider } from '@taiga-ui/kit';
 import { TuiTextfield, TuiError, TuiIcon } from '@taiga-ui/core';
 import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
@@ -17,14 +18,22 @@ import { AsyncPipe } from '@angular/common';
         TuiIcon,
         TuiFieldErrorPipe,
         AsyncPipe,
+        RouterLink,
+
     ],
     templateUrl: './login-form.component.html',
     styleUrl: './login-form.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
+    providers: [
+        tuiValidationErrorsProvider({
+            required: 'Заполните поле',
+            email: 'Введите корректный адрес электронной почты',
+        }),
+    ],
 })
 export class LoginFormComponent {
-    protected readonly form: FormGroup = new FormGroup({
-        email: new FormControl('', Validators.email),
+    protected readonly loginForm: FormGroup = new FormGroup({
+        email: new FormControl('', Validators.compose([Validators.required,  Validators.email])),
         password: new FormControl('', Validators.required)
     });
 }
