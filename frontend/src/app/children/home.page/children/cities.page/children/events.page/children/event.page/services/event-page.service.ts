e901f5e642f5a10eb3fb221b2ce94ad9f0847d13
willie_vonka <nikolai.klimov@urfu.me>
2025-04-26 21:24:35 +0500

    Начал делать сервис для страницы ивентов и получать данные с бэка

--- frontend/src/app/children/home.page/children/cities.page/children/events.page/children/event.page/services/event-page.service.ts
+++ frontend/src/app/children/home.page/children/cities.page/children/events.page/children/event.page/services/event-page.service.ts
@@ -1,11 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../../../../../../../../environment';
+import { IEvent } from '../../../../../../../interfaces/event.interface';
+import { combineLatest, map, Observable } from 'rxjs';
+import { ICity } from '../../../../../../../interfaces/city.interface';
+
 
 @Injectable({
     providedIn: 'root'
 })
 export class EventPageService {
+    private readonly _apiUrl: string = environment.apiUrl;
 
-    constructor() {
-        // This service is currently empty, but can be used for future logic related to the event page.
+    constructor(private _http: HttpClient) {}
+
+    /**
+     *
+     */
+    public getEvent(eventId: string): Observable<IEvent> {
+        return this._http.get<IEvent>(`${this._apiUrl}/event/${eventId}`);
+    }
+
+    /**
+     *
+     */
+    public getCity(cityId: string): Observable<ICity> {
+        return this._http.get<ICity>(`${this._apiUrl}/cities/${cityId}`);
+    }
+
+
+    /**
+     * Generates breadcrumbs for navigation based on the provided city and event observables.
+     * @param city - Observable emitting the city details.
+     * @param event - Observable emitting the event details.
+     * @returns Observable emitting an array of breadcrumb objects with captions and router links.
+     */
+    public getBreadcrumbs(
+        city: Observable<ICity>, 
+        event: Observable<IEvent>
+    ): Observable<Array<{ caption: string; routerLink: string }>> {
+        return combineLatest([city, event]).pipe(
+            map(([c, e]: [ICity, IEvent]) => [
+                { caption: 'Главная', routerLink: '/home' },
+                { caption: 'Города', routerLink: '/home/cities' },
+                { caption: c.name, routerLink: `/home/cities/${c.id}` },
+                { caption: e.title, routerLink: `/home/cities/${c.id}/${e.id}` }
+            ])
+        );
     }
 }
