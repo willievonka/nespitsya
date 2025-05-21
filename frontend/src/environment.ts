14335aaf4d973d34a12a12f0d63b58dbdc3e03e0
willie_vonka <nikolai.klimov@urfu.me>
2025-05-21 17:45:48 +0500

    Рефреш API

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,6 +1,6 @@
 export const environment: { apiUrl: string, authUrl: string, yaMapsApiKey: string, } = {
-    apiUrl: 'https://rigorously-big-darter.cloudpub.ru/api',
-    authUrl: 'https://rigorously-big-darter.cloudpub.ru/auth',
+    apiUrl: 'https://secretly-modern-trout.cloudpub.ru/api',
+    authUrl: 'https://secretly-modern-trout.cloudpub.ru/auth',
     //[ ] TODO: переназначить HTTP Refer в личном кабинете Яндекса после деплоя
     yaMapsApiKey: 'c5b1fe15-5b40-4065-aa0c-28ae7d714d25',
 };
