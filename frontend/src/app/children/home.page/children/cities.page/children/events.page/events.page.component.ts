e49d1afbeb19b2d270af4a37c259ab65127f641a
EduardKostylev <edik03032005@yandex.ru>
2025-05-31 15:49:20 +0500

    Добавил фильтры для событий: место, тип и дата с соответствующими выпадающими списками(в работе)

--- frontend/src/app/children/home.page/children/cities.page/children/events.page/events.page.component.ts
+++ frontend/src/app/children/home.page/children/cities.page/children/events.page/events.page.component.ts
@@ -10,16 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 import { EventsListComponent } from './components/events-list/events-list.component';
 import { TEventsList } from './types/events-list.type';
 import { IEvent } from '../../../../interfaces/event.interface';
+import { TuiButton } from '@taiga-ui/core';
+import { TuiCheckbox } from '@taiga-ui/kit';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
     selector: 'app-events.page',
     imports: [
         CommonModule,
+        FormsModule,
         EventsListComponent,
         TuiBreadcrumbsComponent,
         CityDeclensionPipe,
         TuiOutlineButtonComponent,
+        TuiButton,
+        TuiCheckbox,
     ],
     templateUrl: './events.page.component.html',
     styleUrl: './events.page.component.scss',
@@ -30,9 +36,28 @@ export class EventsPageComponent {
     public events$: Observable<TEventsList>;
     public eventsNumber$: Observable<number>;
     public breadcrumbsItems$: Observable<Array<{ caption: string, routerLink: string }>>;
-    
+    public isPlaceDropdownOpen: boolean = false;
+    public isTypeDropdownOpen: boolean = false;
+    public isDateDropdownOpen: boolean = false;
+    public dateTabs: Array<{ name: string; checked: boolean }> = [
+        { name: 'Все даты', checked: false },
+        { name: 'Сегодня', checked: false },
+        { name: 'Завтра', checked: false },
+        { name: 'На этой неделе', checked: false },
+    ];
+    public typeTabs: Array<{ name: string; checked: boolean }> = [
+        { name: 'Все типы', checked: false },
+        { name: 'Концерты', checked: false },
+        { name: 'Выставки', checked: false },
+    ];
+    public placeTabs: Array<{ name: string; checked: boolean }> = [
+        { name: 'Все места', checked: false },
+        { name: 'Театры', checked: false },
+        { name: 'Музеи', checked: false },
+    ];
+
     constructor(
-        private _eventsPageService: EventsPageService, 
+        private _eventsPageService: EventsPageService,
         private _route: ActivatedRoute,
     ) {
         const cityId: string = this._route.snapshot.paramMap.get('city-id') || '';
@@ -40,8 +65,8 @@ export class EventsPageComponent {
         this.city$ = this._eventsPageService.getCity(cityId);
         this.events$ = this._eventsPageService.getEvents(cityId);
         this.eventsNumber$ = this.events$.pipe(
-            map((events: TEventsList) => 
-                events.reduce((total: number, eventGroup: { events: IEvent[] }) => 
+            map((events: TEventsList) =>
+                events.reduce((total: number, eventGroup: { events: IEvent[] }) =>
                     total + eventGroup.events.length, 0
                 )
             )
