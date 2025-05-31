a4240a738fd65582d8b121675ba2117e823a3b66
willie_vonka <nikolai.klimov@urfu.me>
2025-05-31 13:44:34 +0500

    Сделал удаление ивента из избранного в аккаунте

--- frontend/src/app/children/account.page/components/favorites/favorites.component.ts
+++ frontend/src/app/children/account.page/components/favorites/favorites.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
+import { BehaviorSubject, Observable, switchMap, take } from 'rxjs';
 import { IUser } from '../../../../interfaces/user.interface';
 import { AccountService } from '../../services/account.service';
 import { CommonModule } from '@angular/common';
@@ -22,11 +22,27 @@ import { TuiButton } from '@taiga-ui/core';
 export class FavoritesComponent {
     protected user$: Observable<IUser>;
     protected events$: Observable<IEvent[]>;
+    private _favoritesIds$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
 
     constructor(private _accountService: AccountService) {
         this.user$ = this._accountService.getUser();
-        this.events$ = this.user$.pipe(
-            switchMap(user => this._accountService.getEventsByIds(user.favorites || []))
+
+        this.user$.pipe(take(1)).subscribe(user => {
+            this._favoritesIds$.next(user.favorites || []);
+        });
+
+        this.events$ = this._favoritesIds$.pipe(
+            switchMap(ids => this._accountService.getEventsByIds(ids))
+        );
+    }
+
+    /**
+     * Removes an event from the favorites list by its ID.
+     * @param eventId The ID of the event to remove.
+     */
+    public onEventRemoved(eventId: number): void {
+        this._favoritesIds$.next(
+            this._favoritesIds$.value.filter(id => id !== eventId)
         );
     }
 }
