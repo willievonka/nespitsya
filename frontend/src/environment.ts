b2d205a2eb974cb6ee854aad27e1aa67b628cfd7
Nikolai Klimov <145471275+willievonka@users.noreply.github.com>
2025-07-30 17:02:34 +0500

    Update environment.ts

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,6 +1,1 @@
-export const environment: { apiUrl: string, authUrl: string, yaMapsApiKey: string, } = {
-    apiUrl: 'https://eternally-principal-grasshopper.cloudpub.ru/api',
-    authUrl: 'https://eternally-principal-grasshopper.cloudpub.ru/auth',
-    //[ ] TODO: переназначить HTTP Refer в личном кабинете Яндекса после деплоя
-    yaMapsApiKey: 'c5b1fe15-5b40-4065-aa0c-28ae7d714d25',
-};
+
